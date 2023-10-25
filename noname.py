d7d5602542e0057e483a6c55f14160bbbfeea701
cyhking <cyhking@gmail.com>
2023-10-25 10:07:05 +0900

    파일연습1 완료

--- noname.py
+++ noname.py
@@ -27,3 +27,18 @@
 
 # from 패키지명 import 모듈명
 # from 패키지명 import *
+
+# 클래스
+# class 클래스명(부모)
+# __init__ 생성자
+# __del__ 
+
+#텍스트 파일처리
+# read() 한번에 다 읽음
+# readlines() list로 떨어지므로 for in과 함께 사용
+# readline() 대용량파일처리할때 while과 함께 사용
+# write()
+# tell()
+# seek()
+# print(f.readline(), end="")  end=""로 줄바꿈 제거
+
